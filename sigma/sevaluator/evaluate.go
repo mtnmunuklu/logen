941c45734b0a36b86b50b3ca7cf1a8279e0fe6c9
mtnmunuklu <mtnmunuklu@gmail.com>
2024-02-29 22:19:30 +0300

    Improve RuleEvaluator and Result in Sigma Rule Evaluation
    - Renamed and reorganized fields in Result to be more descriptive.
    - Changed the field names in Result to better represent the evaluated components.
    - Updated the comments in RuleEvaluator for clarity.
    - Adjusted the computation of the final query strings in RuleEvaluator.
    - Modified the commit message for better clarity and conciseness.

--- sigma/sevaluator/evaluate.go
+++ sigma/sevaluator/evaluate.go
@@ -34,26 +34,26 @@ func ForRule(rule sigma.Rule, options ...Option) *RuleEvaluator {
 // Result represents the evaluation result of a Sigma rule.
 // It contains the search, condition, aggregation, and query results of the rule evaluation.
 type Result struct {
-	SearchResults      map[string][]string // The map of search identifiers to their result values
-	ConditionResults   map[int][]string    // The map of condition indices to their result values
-	AggregationResults map[int]string      // The map of aggregation indices to their result values
-	QueryResults       map[int]string      // The map of query indices to their result values
+	Searches    map[string][]string // The map of search identifiers to their result values
+	Conditions  map[int][]string    // The map of condition indices to their result values
+	SourceTypes map[int]string      // The map of sourcetype indices to their result values
+	Queries     map[int]string      // The map of query indices to their result values
 }
 
 // This function returns a Result object containing the evaluation results for the rule's Detection field.
 // It uses the evaluateSearch, evaluateSearchExpression and evaluateAggregationExpression functions to compute the results.
 func (rule RuleEvaluator) Alters(ctx context.Context) (Result, error) {
 	result := Result{
-		SearchResults:      make(map[string][]string),
-		ConditionResults:   make(map[int][]string),
-		AggregationResults: make(map[int]string),
-		QueryResults:       make(map[int]string),
+		Searches:    make(map[string][]string),
+		Conditions:  make(map[int][]string),
+		SourceTypes: make(map[int]string),
+		Queries:     make(map[int]string),
 	}
 
 	// Evaluate all the search expressions in the Detection field and store the results in the SearchResults map of the result object.
 	for identifier, search := range rule.Detection.Searches {
 		var err error
-		result.SearchResults[identifier], err = rule.evaluateSearch(ctx, search)
+		result.Searches[identifier], err = rule.evaluateSearch(ctx, search)
 		if err != nil {
 			return Result{}, fmt.Errorf("error evaluating search %s: %w", identifier, err)
 		}
@@ -62,23 +62,16 @@ func (rule RuleEvaluator) Alters(ctx context.Context) (Result, error) {
 	// Evaluate all the search expressions in the Detection field's Conditions array and store the results in the ConditionResults map of the result object.
 	// If a condition has an Aggregation field, also evaluate it and store the result in the AggregationResults map of the result object.
 	for conditionIndex, condition := range rule.Detection.Conditions {
-		result.ConditionResults[conditionIndex] = rule.evaluateSearchExpression(condition.Search, []string{}, true)
-		if condition.Aggregation != nil {
-			var err error
-			result.AggregationResults[conditionIndex], err = rule.evaluateAggregationExpression(ctx, conditionIndex, condition.Aggregation)
-			if err != nil {
-				return Result{}, err
-			}
-		}
+		result.Conditions[conditionIndex] = rule.evaluateSearchExpression(condition.Search, []string{}, true)
 	}
 
 	// Combine the search results and condition results to form the final query strings for each condition.
 	// The query strings are stored in the QueryResults map of the result object.
-	for i, conditionResult := range result.ConditionResults {
+	for i, conditionResult := range result.Conditions {
 		conditionList := make([]string, 0, len(conditionResult))
 		for _, condition := range conditionResult {
 			// If the condition matches any search identifier, replace it with the corresponding search results
-			if value, ok := result.SearchResults[condition]; ok {
+			if value, ok := result.Searches[condition]; ok {
 				if len(conditionResult) > 1 {
 					conditionList = append(conditionList, "("+strings.Join(value, " and ")+")")
 				} else if len(value) > 1 {
@@ -92,19 +85,14 @@ func (rule RuleEvaluator) Alters(ctx context.Context) (Result, error) {
 			}
 		}
 
-		// If the condition has an aggregation, add the aggregation to the final query string
-		if result.AggregationResults[i] != "" {
-			aggregationResult := strings.Split(result.AggregationResults[i], "|")
-			for j, aggregation := range aggregationResult {
-				if j == 0 {
-					result.QueryResults[i] = aggregation + strings.Join(conditionList, "")
-				} else {
-					result.QueryResults[i] += " " + aggregation
-				}
-			}
-		} else {
-			// If the condition doesn't have an aggregation, add the conditionList to the final query string
-			result.QueryResults[i] = strings.Join(conditionList, "")
+		// add the conditionList to the final query string
+		result.Queries[i] = strings.Join(conditionList, "")
+
+		// Add the sourcetype condition to the final query string, if applicable
+		if rule.Logsource.Product != "" && rule.Logsource.Service != "" {
+			result.SourceTypes[i] = rule.Logsource.Product + " " + rule.Logsource.Service
+		} else if rule.Logsource.Product != "" && rule.Logsource.Service == "" {
+			result.SourceTypes[i] = rule.Logsource.Product
 		}
 	}
 
